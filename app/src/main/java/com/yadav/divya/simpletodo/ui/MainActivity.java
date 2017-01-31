9517446ac9ff1e508c9b5a0d76806367a019f31f
Divya Yadav <divyayadav1606@gmail.com>
2017-01-31 00:28:53 -0800

    Swipe to Delete & Mark as complete

--- app/src/main/java/com/yadav/divya/simpletodo/ui/MainActivity.java
+++ app/src/main/java/com/yadav/divya/simpletodo/ui/MainActivity.java
@@ -21,14 +21,15 @@ public class MainActivity extends AppCompatActivity implements
     private TodoAdapter mtodoAdapter;
     private Cursor mCursor;
     private ListView mList;
+    private DbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DbHelper dbHelper = new DbHelper(this.getApplicationContext());
-        mCursor = dbHelper.getWritableDatabase().rawQuery("SELECT  * FROM TASK_LIST", null);
+        mDbHelper = new DbHelper(this.getApplicationContext());
+        mCursor = mDbHelper.getWritableDatabase().rawQuery("SELECT  * FROM TASK_LIST", null);
 
         mList = (ListView) findViewById(R.id.list);
         mtodoAdapter = new TodoAdapter(this, mCursor, 0);
@@ -51,14 +52,13 @@ public class MainActivity extends AppCompatActivity implements
         });
     }
 
-    private void updateDb(String task) {
-        DbHelper dbHelper = new DbHelper(this);
+    private void addToDb(String task) {
         ContentValues values = new ContentValues();
         values.clear();
 
         values.put("task", task);
         values.put("status", "0");
-        dbHelper.getWritableDatabase().insertWithOnConflict("TASK_LIST", null, values, SQLiteDatabase.CONFLICT_IGNORE);
+        mDbHelper.getWritableDatabase().insertWithOnConflict("TASK_LIST", null, values, SQLiteDatabase.CONFLICT_IGNORE);
     }
 
     @Override
@@ -74,8 +74,30 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onSwipe(int[] position, SwipeDirection[] direction) {
+    public void onSwipe(int[] positionList, SwipeDirection[] directionList) {
+        for (int i = 0; i < positionList.length; i++) {
+            SwipeDirection direction = directionList[i];
+            int position = positionList[i];
+            String where = "";
 
+            Cursor c = (Cursor) mAdapter.getItem(position);
+            where = "_ID = " + c.getString(0);
+
+            switch (direction) {
+                case DIRECTION_FAR_LEFT:
+                    ContentValues values = new ContentValues();
+                    values.put("status", "1");
+                    mDbHelper.getWritableDatabase().update("TASK_LIST", values, where, null);
+                    break;
+                case DIRECTION_NORMAL_LEFT:
+                case DIRECTION_NORMAL_RIGHT:
+                    break;
+                case DIRECTION_FAR_RIGHT:
+                    mDbHelper.getWritableDatabase().delete("TASK_LIST", where, null);
+                    refreshUI();
+                    break;
+            }
+        }
     }
 
     private void showEditDialog() {
@@ -84,13 +106,17 @@ public class MainActivity extends AppCompatActivity implements
         dFragment.setFinishDialogListener(new AddEditTask.AddEditTaskListener() {
             @Override
             public void onFinishDialog(String task) {
-                updateDb(task);
-                mCursor.requery();
-                mAdapter.notifyDataSetChanged();
-                mList.setAdapter(mAdapter);
+                addToDb(task);
+                refreshUI();
             }
         });
 
         dFragment.show(getSupportFragmentManager(), "");
+    }
+
+    private void refreshUI() {
+        mCursor.requery();
+        mAdapter.notifyDataSetChanged();
+        mList.setAdapter(mAdapter);
     }
 }
